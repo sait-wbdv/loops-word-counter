868242c096c2a15b056564989b2323faba679570
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 16:09:15 -0600

    spoiler code added that demonstrate for of loop

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 "use strict";
 
-const sentence =
-  "The quick brown fox jumps over the lazy dog dog over the brown.";
+const sentence = "The quick brown fox jumps over the lazy dog.";
 
 // 1. split the sentence up at the spaces and make sure the letters are all lowerCase
+const words = sentence.toLowerCase().split(" ");
 
-// 2 add the following replace method with a regular expression to the split code to remove punctuation: .replace(/[.,!>]/g, "")
+// 2 add this replace method with a regular expression to the split code to remove punctuation
+/* .replace(/[.,!>]/g, "") */
+let wordCounts = {};
 
-// 3. initialize an object variable that you can add values to. the conditional code supplied expects this to be called "wordCounts"
+// 3. initialize an object variable that you can add values to
 
-// 4. iterate through the words in the array and execute the following conditional code for each word
-
-// NOTE: depending on the type of loop that you use, where you initialize the word variable as seen below will change
-
-if (wordCounts[word]) {
-  /* if the word is already a key in the object */
-  wordCounts[word]++; /* increment it's value count */
-} else {
-  /* if the word doesn't exist yet, add it and set it's value to 1 */
-  wordCounts[word] = 1;
+// 4. iterate through the words in the array
+for (const word of words) {
+  // if the word exists, add to it's count
+  if (wordCounts[word]) {
+    wordCounts[word]++;
+    // if it's the first encounter, add the word with the value of 1
+  } else {
+    wordCounts[word] = 1;
+  }
 }
+
+console.log(wordCounts);
